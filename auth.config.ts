ae5905d25fde61f873dd26af162acc736d05f038
Jetro <jetrokeplergomes@gmail.com>
2024-10-17 08:30:17 -0300

    Update on user authentication flow.

--- auth.config.ts
+++ auth.config.ts
@@ -5,57 +5,21 @@ import { sql } from '@vercel/postgres';
 import type { NextAuthConfig } from "next-auth";
 
 export default {
-  providers: [
-    GithubProvider({
-      clientId: process.env.GITHUB_ID as string,
-      clientSecret: process.env.GITHUB_SECRET as string,
-      authorization: {
-        params: { scope: 'read:user user:email' },
-      },
-      allowDangerousEmailAccountLinking: true,
-    }),
-    Credentials({
-      credentials: {
-        email: {
-          label: 'Email',
-          type: 'email',
-          placeholder: 'email@exemplo.com.br',
-        },
-        password: { label: 'Senha', type: 'password' },
-      },
-      async authorize(credentials) {
-        const email = credentials?.email as string;
-        const password = credentials?.password as string;
-        
-        if (!email || !password) {
-          return null;
-        }
-
-        try {
-          // Realizando a consulta no banco de dados diretamente com `sql`
-          const result = await sql`
-            SELECT * FROM users WHERE email = ${email};
-          `;
-
-          const user = result.rows[0];
-
-          if (!user) {
-            return null; // Usuário não encontrado
-          }
-
-          // Comparando a senha armazenada no banco com a senha fornecida
-          const passwordMatch = compareSync(password, user.password ?? '');
-          
-          if (!passwordMatch) {
-            return null; // Senha incorreta
-          }
-
-          return user;
-        } catch (error) {
-          console.error('Erro na autenticação:', error);
-          return null;
-        }
-      },
-    }),
-  ],
+  pages: {
+    signIn: '/signin',
+  },
+  callbacks: {
+    authorized({ auth, request: { nextUrl } }) {
+      const isLoggedIn = !!auth?.user;
+      const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
+      if (isOnDashboard) {
+        if (isLoggedIn) return true;
+        return false; // Redirect unauthenticated users to login page
+      } else if (isLoggedIn) {
+        return Response.redirect(new URL('/dashboard', nextUrl));
+      }
+      return true;
+    },
+  },
+  providers: [],
 } satisfies NextAuthConfig;
--- auth.ts
+++ auth.ts
@@ -1,5 +1,10 @@
 import NextAuth from 'next-auth';
 import authConfig from './auth.config';
+import Credentials from 'next-auth/providers/credentials';
+import GithubProvider from "next-auth/providers/github";
+import { sql } from '@vercel/postgres';
+import type { NextAuthConfig } from "next-auth";
+import { compareSync } from 'bcrypt-ts';
 
 export const {handlers, signIn, signOut, auth,} = NextAuth({
 
@@ -7,4 +12,57 @@ export const {handlers, signIn, signOut, auth,} = NextAuth({
     strategy:'jwt'
   },
   ...authConfig,
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID as string,
+      clientSecret: process.env.GITHUB_SECRET as string,
+      authorization: {
+        params: { scope: 'read:user user:email' },
+      },
+      allowDangerousEmailAccountLinking: true,
+    }),
+    Credentials({
+      credentials: {
+        email: {
+          label: 'Email',
+          type: 'email',
+          placeholder: 'email@exemplo.com.br',
+        },
+        password: { label: 'Senha', type: 'password' },
+      },
+      async authorize(credentials) {
+        const email = credentials?.email as string;
+        const password = credentials?.password as string;
+        
+        if (!email || !password) {
+          return null;
+        }
+  
+        try {
+          // Realizando a consulta no banco de dados diretamente com `sql`
+          const result = await sql`
+            SELECT * FROM users WHERE email = ${email};
+          `;
+  
+          const user = result.rows[0];
+  
+          if (!user) {
+            return null; // Usuário não encontrado
+          }
+  
+          // Comparando a senha armazenada no banco com a senha fornecida
+          const passwordMatch = compareSync(password, user.password ?? '');
+          
+          if (!passwordMatch) {
+            return null; // Senha incorreta
+          }
+  
+          return user;
+        } catch (error) {
+          console.error('Erro na autenticação:', error);
+          return null;
+        }
+      },
+    }),
+  ], // Add providers with an empty array for now
 });

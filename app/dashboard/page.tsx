0144453a76a1ae2bfe4ee1c3cf8264a120fe9073
Jetro <jetrokeplergomes@gmail.com>
2024-10-17 21:16:28 -0300

    Fix on 'Log out' button.

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -54,46 +54,12 @@ export default async function DashboardPage() {
             <Button variant="outline" size="icon">
               <Bell className="h-4 w-4" />
             </Button>
-            <DropdownMenu>
-              <DropdownMenuTrigger asChild>
-                <Button variant="ghost" className="relative h-8 w-8 rounded-full">
-                  <Avatar className="h-8 w-8">
-                    <AvatarImage src="/placeholder.svg?height=32&width=32" alt={user?.name ?? ''} />
-                    <AvatarFallback>{user?.name ?? ''.charAt(0)}</AvatarFallback>
-                  </Avatar>
+            <form action={signout}>
+                <Button variant="outline" className="flex items-center justify-center">
+                    <LogOut className="mr-2 h-4 w-4" />
+                    <span>Log out</span>
                 </Button>
-              </DropdownMenuTrigger>
-              <DropdownMenuContent className="w-56" align="end" forceMount>
-                <DropdownMenuLabel className="font-normal">
-                  <div className="flex flex-col space-y-1">
-                    <p className="text-sm font-medium leading-none">{user?.name ?? ''}</p>
-                    <p className="text-xs leading-none text-muted-foreground">{user?.email ?? ''}</p>
-                  </div>
-                </DropdownMenuLabel>
-                <DropdownMenuSeparator />
-                <DropdownMenuItem>
-                  <Client className="mr-2 h-4 w-4" />
-                  <span>Profile</span>
-                </DropdownMenuItem>
-                <DropdownMenuItem>
-                  <CreditCard className="mr-2 h-4 w-4" />
-                  <span>Billing</span>
-                </DropdownMenuItem>
-                <DropdownMenuItem>
-                  <Settings className="mr-2 h-4 w-4" />
-                  <span>Settings</span>
-                </DropdownMenuItem>
-                <DropdownMenuSeparator />
-                <DropdownMenuItem>
-                  <form action={signout}>
-                    <button className="flex">
-                      <LogOut className="mr-2 h-4 w-4" />
-                      <span>Log out</span>
-                    </button>
-                  </form>
-                </DropdownMenuItem>
-              </DropdownMenuContent>
-            </DropdownMenu>
+            </form>
           </div>
         </header>
         <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-4">
